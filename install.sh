c7333bbd97b1759be3658dd56f3517f67f324dd9
Jenn <benminedew@gmail.com>
2026-03-01 11:56:47 -0800

    More script tweaks <3

--- install.sh
+++ install.sh
@@ -4,14 +4,15 @@ sudo apt update
 sudo apt upgrade -y
 cd ~
 echo "Installing node-red"
-bash <(curl -sL https://github.com/node-red/linux-installers/releases/latest/download/update-nodejs-and-nodered-deb)
+bash <(curl -sL https://github.com/node-red/linux-installers/releases/latest/download/update-nodejs-and-nodered-deb) --confirm-root --confirm-install --confirm-pi
 cd ~
 echo "Installing mosquitto"
 sudo apt install mosquitto mosquitto-clients -y
 echo "Enabling node-red service"
 sudo systemctl enable nodered.service
 sudo systemctl stop nodered.service
-sudo cp ~/MeshBridge/flows.json ~/.node-red/
+echo "Copying flow"
+echo sudo cp ~/MeshBridge/flows.json ~/.node-red/
 sudo systemctl start nodered.service
 echo "Enabling mosquitto service"
 sudo systemctl enable mosquitto
