e1a1965391e1b32a6724a7e3232ce3e2e4c16f48
NotAnExpert490 <106001035+NotAnExpert490@users.noreply.github.com>
2026-03-01 20:43:29 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo "Installing node-red"
 bash <(curl -sL https://github.com/node-red/linux-installers/releases/latest/download/update-nodejs-and-nodered-deb) --nodered-user=$USER --confirm-install --confirm-pi
 cd ~
 echo "Installing mosquitto"
-sudo apt install mosquitto mosquitto-clients -y
+apt install mosquitto mosquitto-clients -y
 echo "Enabling node-red service"
 echo "Copying flow"
 cp ~/MeshBridge/flows.json ~/.node-red/
@@ -17,13 +17,13 @@ echo "Enabling mosquitto service"
 sudo systemctl enable mosquitto
 cd ~
 echo "Installing python"
-sudo apt install python3-pip -y
-sudo apt install pipx -y
+apt install python3-pip -y
+apt install pipx -y
 echo "Installing meshcore-cli"
-sudo pipx install meshcore-cli
+pipx install meshcore-cli
 cd ~
 echo "Installing meshtastic-cli"
-sudo pipx install "meshtastic[cli]"
+pipx install "meshtastic[cli]"
 pipx ensurepath
 sudo reboot
 
